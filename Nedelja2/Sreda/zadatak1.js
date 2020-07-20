7f2a8aabf9553b0c58f0ea058247e9045682ced6
nbrajic <brajic.nevena@gmail.com>
2020-07-20 16:54:33 +0200

    Zadatak1

---
+++ Nedelja2/Sreda/zadatak1.js
@@ -0,0 +1,18 @@
+// За два броја (а и б) исписати њихов: Збир,разлику,производ,количник,остатак при дељењу,степен (а^b)
+
+let a = 9;
+console.log ("a =", a);
+
+let b = 2;
+console.log ("b =", b);
+
+console.log ("a + b =", a + b);
+console.log ("a - b =", a - b);
+console.log ("a * b =", a * b);
+console.log ("a / b =", a / b);
+console.log ("a % b =", a % b);
+console.log ("a ^ b = ", Math.pow(a , b));
+
+
+
+
